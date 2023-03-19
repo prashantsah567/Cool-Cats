7dafdc2523e3889f8abef6cf73ed76b5c92d20ec
Prashant Sah <prashantsah567@gmail.com>
2023-03-18 23:28:12 -0400

    other properties added and background image changed

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -4,14 +4,23 @@ import RightAddingBan from './RightAddingBan';
 import './MainView.css';
 
 const MainView = () => {
-    const [imageLink, setImageLink] = useState('src/assets/mars.jpg');
+    const [imageLink, setImageLink] = useState('src/assets/cat1.jpeg');
+    const [name, setName] = useState('Name');
+    const [origin, setOrigin] = useState('USA');
+    const [lifespan, setlifeSpan] = useState('lifespan');
+    const [weight, setWeight] = useState('weight');
 
     const url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_1Rg1VoeW1VUHhwEXBuDJP9PPCWw59UwD0V1p3yC9cUBxmcaGFljiGHb5ViAUYchA';
     const submitDiscoverBtn = async () =>{
         const response = await fetch(url);
         const data = await response.json();
-        setImageLink(data[Math.floor(Math.random() * 10)].url);
-        console.log(data[Math.floor(Math.random() * 10)].url);
+        const randomSelector = Math.floor(Math.random() * 10);
+        setImageLink(data[randomSelector].url);
+        document.querySelectorAll('.hidden').forEach(elem => elem.style.display = 'block');
+        setName(data[randomSelector].breeds[0].name);
+        setOrigin(data[randomSelector].breeds[0].origin);
+        setlifeSpan(data[randomSelector].breeds[0].life_span);
+        setWeight(data[randomSelector].breeds[0].weight.imperial);
         console.log(data);
     }
 
@@ -27,14 +36,13 @@ const MainView = () => {
                     <p>Cats around the world</p>
                     <p></p>
                     <div className='selectCharacter'>
-                        <div className="char1">RHAZ</div>
-                        <div className="char2">CHEMCAM</div>
-                        <div className="char3">MAHIL</div>
-                        <div className="char4">NAVCAM</div>
-                        <div className="char5">MINITES</div>
+                        <button className="button-1 hidden" role="button">{name}</button>
+                        <button className="button-1 hidden" role="button">{origin}</button>
+                        <button className="button-1 hidden" role="button">{lifespan}</button>
+                        <button className="button-1 hidden" role="button">{weight}</button>
                     </div>
                     <p></p>
-                    <img src={imageLink} width="500px" height="500px"/>
+                    <img src={imageLink} width="500px" height="450px" className="hidden" id="hiddenImg"/>
                     <p></p>
                     <button className="discoverBtn" onClick={submitDiscoverBtn}>🔄Discover</button>
                 </div>
